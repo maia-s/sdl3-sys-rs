dc590ee7999ac02874c1f7debf00a10a7aab719e
Maia <66437537+maia-s@users.noreply.github.com>
2024-08-14 21:28:43 +0200

    support macros

--- sdl3-sys/src/lib.rs
+++ sdl3-sys/src/lib.rs
@@ -1,1 +1,17 @@
+#![no_std]
 
+// This macro is used to apply attributes (like cfg) to a group of items. Wrap
+// the items in a call to this macro and apply the attributes to the macro call
+macro_rules! emit {
+    ($($tt:tt)*) => { $($tt)* };
+}
+
+// The calling convention for SDL on non-GNU Windows uses "cdecl" instead of "C"
+#[cfg(all(target_family = "windows", not(target_env = "gnu")))]
+macro_rules! extern_sdlcall {
+    ($($tt:tt)*) => { extern "cdecl" $($tt)* };
+}
+#[cfg(not(all(target_family = "windows", not(target_env = "gnu"))))]
+macro_rules! extern_sdlcall {
+    ($($tt:tt)*) => { extern "C" $($tt)* };
+}
