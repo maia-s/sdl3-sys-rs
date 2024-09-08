a5b70c54049a0488ed50aed2aa80629ea6ad680a
Maia <66437537+maia-s@users.noreply.github.com>
2024-09-08 19:09:31 +0200

    single colon makes the crate work from 1.75 instead of 1.77

--- sdl3-sys/build.rs
+++ sdl3-sys/build.rs
@@ -7,9 +7,9 @@ fn main() {
         + cfg!(feature = "assert-level-paranoid") as usize;
     if enabled_assert_levels == 0 {
         if env::var("DEBUG").unwrap() == "true" {
-            println!(r#"cargo::rustc-cfg=feature="assert-level-debug""#);
+            println!(r#"cargo:rustc-cfg=feature="assert-level-debug""#);
         } else {
-            println!(r#"cargo::rustc-cfg=feature="assert-level-release""#);
+            println!(r#"cargo:rustc-cfg=feature="assert-level-release""#);
         }
     }
 }
