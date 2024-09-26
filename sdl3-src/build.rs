b09cab90e41f306b69d57a06ded4d3e808e43192
Maia <66437537+maia-s@users.noreply.github.com>
2024-09-26 18:08:27 +0200

    don't build/link SDL on docs.rs (cfg(doc) doesn't work in build scripts)

--- sdl3-src/build.rs
+++ sdl3-src/build.rs
@@ -7,23 +7,28 @@ use std::{
 };
 
 fn main() {
-    let mut config = Config::new("SDL");
-
-    let build_static = cfg!(feature = "build-static");
-    let build_framework = cfg!(feature = "build-framework");
-    if build_static {
-        config.define("SDL_STATIC", "ON");
-        config.define("SDL_SHARED", "OFF");
-    } else if build_framework {
-        config.define("SDL_FRAMEWORK", "ON");
+    let build = if env::var("DOCS_RS").is_ok() {
+        // don't build SDL on docs.rs
+        std::path::PathBuf::new()
     } else {
-        config.define("SDL_STATIC", "OFF");
-        config.define("SDL_SHARED", "ON");
-    }
+        let mut config = Config::new("SDL");
 
-    let mut build = config.build();
-    build.push("lib");
-    let build = build.canonicalize().unwrap();
+        let build_static = cfg!(feature = "build-static");
+        let build_framework = cfg!(feature = "build-framework");
+        if build_static {
+            config.define("SDL_STATIC", "ON");
+            config.define("SDL_SHARED", "OFF");
+        } else if build_framework {
+            config.define("SDL_FRAMEWORK", "ON");
+        } else {
+            config.define("SDL_STATIC", "OFF");
+            config.define("SDL_SHARED", "ON");
+        }
+
+        let mut build = config.build();
+        build.push("lib");
+        build.canonicalize().unwrap()
+    };
 
     let mut out = BufWriter::new(
         File::create(Path::new(&env::var_os("OUT_DIR").unwrap()).join("config.rs")).unwrap(),
