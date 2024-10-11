9e847490152aeeea59900a7353e84d5f69df65b4
Maia <66437537+maia-s@users.noreply.github.com>
2024-10-11 18:00:51 +0200

    use the canonical path separator on windows

--- sdl3-src/src/lib.rs
+++ sdl3-src/src/lib.rs
@@ -2,4 +2,7 @@
 #![doc = include_str!("../README.md")]
 
 /// Location of the SDL 3 source code
+#[cfg(not(windows))]
 pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/SDL");
+#[cfg(windows)]
+pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\SDL");
