2e32355cf1e220fc541dffd88608aa918e54e98c
Maia <66437537+maia-s@users.noreply.github.com>
2024-11-10 01:45:43 +0100

    run cargo check on 1.79 and nightly

--- generate-and-check.sh
+++ generate-and-check.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 cargo run -p sdl3-sys-gen --release
-DOCS_RS=1 cargo check -p sdl3-sys --all-features
+DOCS_RS=1 cargo +1.79.0 check -p sdl3-sys
+DOCS_RS=1 cargo +nightly check -p sdl3-sys --all-features
