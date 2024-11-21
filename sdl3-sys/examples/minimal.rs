43eb83a73ccf01a750746741991b83ceea9b7724
Maia <66437537+maia-s@users.noreply.github.com>
2024-11-21 01:24:58 +0100

    minimal example: return a result from main instead of panicing on failure

--- sdl3-sys/examples/minimal.rs
+++ sdl3-sys/examples/minimal.rs
@@ -1,20 +1,15 @@
 use core::ffi::CStr;
-
 use sdl3_sys::{
     error::SDL_GetError,
     init::{SDL_Init, SDL_Quit, SDL_INIT_VIDEO},
 };
 
-fn main() {
+fn main() -> Result<(), &'static CStr> {
     if unsafe { SDL_Init(SDL_INIT_VIDEO) } {
         println!("Successfully initialized SDL!");
-        unsafe {
-            SDL_Quit();
-        }
+        unsafe { SDL_Quit() };
+        Ok(())
     } else {
-        panic!(
-            "{}",
-            unsafe { CStr::from_ptr(SDL_GetError()) }.to_string_lossy()
-        );
+        Err(unsafe { CStr::from_ptr(SDL_GetError()) })
     }
 }
