ed59a6057f17ff1aaf013bfa4bf60a915ad57750
Maia <66437537+maia-s@users.noreply.github.com>
2024-12-28 20:47:15 +0100

    update SDL to 3.1.6-preview-500-gc030e6f78

--- sdl3-src/src/lib.rs
+++ sdl3-src/src/lib.rs
@@ -8,7 +8,7 @@ pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/SDL");
 pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\SDL");
 
 /// Revision
-pub const REVISION: &str = "SDL3-preview-3.1.6-491-g1a466b554";
+pub const REVISION: &str = "SDL3-preview-3.1.6-500-gc030e6f78";
 
 /// Version part of the revision
 pub const VERSION: &str = "3.1.6";
@@ -20,7 +20,7 @@ pub const REVISION_TAG: &str = "preview-3.1.6";
 pub const REVISION_TAG_BASE: &str = "preview";
 
 /// Offset from tag part of the revision
-pub const REVISION_OFFSET: &str = "491";
+pub const REVISION_OFFSET: &str = "500";
 
 /// Hash part of the revision
-pub const REVISION_HASH: &str = "g1a466b554";
+pub const REVISION_HASH: &str = "gc030e6f78";
--- sdl3-sys/src/generated/revision.rs
+++ sdl3-sys/src/generated/revision.rs
@@ -17,7 +17,7 @@ apply_cfg!(#[cfg(doc)] => {
 });
 
 apply_cfg!(#[cfg(not(doc))] => {
-    pub const SDL_REVISION: *const ::core::ffi::c_char = c"SDL3-preview-3.1.6-491-g1a466b554".as_ptr();
+    pub const SDL_REVISION: *const ::core::ffi::c_char = c"SDL3-preview-3.1.6-500-gc030e6f78".as_ptr();
 
 });
 
