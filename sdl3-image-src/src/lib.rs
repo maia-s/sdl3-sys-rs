3ee25f111c91928fef58d4889c0d2e96e64cf0b0
Maia <66437537+maia-s@users.noreply.github.com>
2025-02-02 13:34:11 +0100

    update SDL_image to 3.2.0

--- sdl3-image-src/src/lib.rs
+++ sdl3-image-src/src/lib.rs
@@ -8,19 +8,19 @@ pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/SDL_image");
 pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\SDL_image");
 
 /// Revision
-pub const REVISION: &str = "SDL3_image-prerelease-3.1.1-12-gb56698cc";
+pub const REVISION: &str = "SDL3_image-release-3.2.0";
 
 /// Version part of the revision
-pub const VERSION: &str = "3.1.1";
+pub const VERSION: &str = "3.2.0";
 
 /// Tag part of the revision
-pub const REVISION_TAG: &str = "prerelease-3.1.1";
+pub const REVISION_TAG: &str = "release-3.2.0";
 
 /// Tag part of the revision without version
-pub const REVISION_TAG_BASE: &str = "prerelease";
+pub const REVISION_TAG_BASE: &str = "release";
 
 /// Offset from tag part of the revision
-pub const REVISION_OFFSET: &str = "12";
+pub const REVISION_OFFSET: &str = "0";
 
 /// Hash part of the revision
-pub const REVISION_HASH: &str = "gb56698cc";
+pub const REVISION_HASH: &str = "g4a762bdf";
--- sdl3-image-sys/README.md
+++ sdl3-image-sys/README.md
@@ -1,3 +1,3 @@
 # sdl3-image-sys
 
-This version of `sdl3-image-sys` has bindings for SDL_image version `3.1.1-prerelease-12-gb56698cc` and earlier.
+This version of `sdl3-image-sys` has bindings for SDL_image version `3.2.0` and earlier.
