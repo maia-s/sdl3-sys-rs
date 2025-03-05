4dd346a95b20e52d02c8b9b8fb22eb954d53ff52
Maia <66437537+maia-s@users.noreply.github.com>
2025-03-05 14:40:47 +0100

    update SDL to 3.2.8

--- sdl3-src/src/lib.rs
+++ sdl3-src/src/lib.rs
@@ -8,13 +8,13 @@ pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/SDL");
 pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\SDL");
 
 /// Revision
-pub const REVISION: &str = "SDL3-release-3.2.6";
+pub const REVISION: &str = "SDL3-release-3.2.8";
 
 /// Version part of the revision
-pub const VERSION: &str = "3.2.6";
+pub const VERSION: &str = "3.2.8";
 
 /// Tag part of the revision
-pub const REVISION_TAG: &str = "release-3.2.6";
+pub const REVISION_TAG: &str = "release-3.2.8";
 
 /// Tag part of the revision without version
 pub const REVISION_TAG_BASE: &str = "release";
@@ -23,4 +23,4 @@ pub const REVISION_TAG_BASE: &str = "release";
 pub const REVISION_OFFSET: &str = "0";
 
 /// Hash part of the revision
-pub const REVISION_HASH: &str = "g65864190c";
+pub const REVISION_HASH: &str = "gf6864924f";
