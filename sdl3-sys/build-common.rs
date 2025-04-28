85a14c9f3af7d52c6ce01ea0fcb1193334b887d6
Maia <66437537+maia-s@users.noreply.github.com>
2025-04-28 17:01:31 +0200

    fix bad merge

--- sdl3-sys/build-common.rs
+++ sdl3-sys/build-common.rs
@@ -262,7 +262,10 @@ fn top_level_cargo_target_dir() -> std::path::PathBuf {
 fn find_and_output_cmake_dir_metadata(out_dir: &std::path::Path) -> Result<(), Box<dyn Error>> {
     use std::path::{Path, PathBuf};
     fn try_dir(dir: &Path) -> bool {
-        if dir.join(format!("{lib_name}Config.cmake")).exists() {
+        if dir
+            .join(format!("{}Config.cmake", config("lib_name")))
+            .exists()
+        {
             println!("cargo::metadata=CMAKE_DIR={}", dir.display());
             true
         } else {
