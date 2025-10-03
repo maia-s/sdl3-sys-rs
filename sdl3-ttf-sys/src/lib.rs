61c3cf9160658b3b89af76a021f9296ee612b18f
Maia <maia.noreply@runbox.com>
2025-10-03 21:41:27 +0200

    doc_auto_cfg was removed from nightly

--- sdl3-ttf-sys/src/lib.rs
+++ sdl3-ttf-sys/src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-#![cfg_attr(all(feature = "nightly", doc), feature(doc_auto_cfg))] // https://github.com/rust-lang/rust/issues/43781
 #![cfg_attr(all(feature = "nightly", doc), feature(doc_cfg))] // https://github.com/rust-lang/rust/issues/43781
 #![doc = include_str!("../README.md")]
 
