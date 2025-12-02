8dfb4b8dff69fc854d1b3ba61e92c3170952fc4b
Maia <maia.noreply@runbox.com>
2025-12-02 22:19:56 +0100

    update SDL to 3.2.30

--- sdl3-src/src/lib.rs
+++ sdl3-src/src/lib.rs
@@ -8,13 +8,13 @@ pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/SDL");
 pub const SOURCE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\SDL");
 
 /// Revision
-pub const REVISION: &str = "SDL-release-3.2.28";
+pub const REVISION: &str = "SDL-release-3.2.30";
 
 /// Version part of the revision
-pub const VERSION: &str = "3.2.28";
+pub const VERSION: &str = "3.2.30";
 
 /// Tag part of the revision
-pub const REVISION_TAG: &str = "release-3.2.28";
+pub const REVISION_TAG: &str = "release-3.2.30";
 
 /// Tag part of the revision without version
 pub const REVISION_TAG_BASE: &str = "release";
@@ -23,4 +23,4 @@ pub const REVISION_TAG_BASE: &str = "release";
 pub const REVISION_OFFSET: &str = "0";
 
 /// Hash part of the revision
-pub const REVISION_HASH: &str = "g7f3ae3d57";
+pub const REVISION_HASH: &str = "gf5e5f6588";
--- sdl3-sys/src/generated/revision.rs
+++ sdl3-sys/src/generated/revision.rs
@@ -17,7 +17,7 @@ apply_cfg!(#[cfg(doc)] => {
 });
 
 apply_cfg!(#[cfg(not(doc))] => {
-    pub const SDL_REVISION: *const ::core::ffi::c_char = c"SDL-release-3.2.28".as_ptr();
+    pub const SDL_REVISION: *const ::core::ffi::c_char = c"SDL-release-3.2.30".as_ptr();
 
 });
 
